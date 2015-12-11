82f65fc3fdce63bed0aa1ded801445db35a3c373
Tim Gesekus <tim.gesekus@dfs.de>
2015-12-11 17:57:26 +0100

    NEWSAS-829 Start of continously monitoring process

--- src/main/java/de/dfs/servicemonitor/monitor/WebServiceHealthMonitor.java
+++ src/main/java/de/dfs/servicemonitor/monitor/WebServiceHealthMonitor.java
@@ -1,13 +1,76 @@
 package de.dfs.servicemonitor.monitor;
 
-import akka.actor.AbstractActor;
+import java.util.concurrent.TimeUnit;
 
-public class WebServiceHealthMonitor extends AbstractActor {
-  public static class MonitorHealth {
+import akka.actor.AbstractActor;
+import akka.actor.ActorContext;
+import akka.actor.ActorRef;
+import akka.actor.Scheduler;
+import akka.dispatch.Dispatcher;
+import akka.japi.pf.ReceiveBuilder;
+import play.libs.F.Promise;
+import play.libs.ws.WSClient;
+import play.libs.ws.WSRequest;
+import play.libs.ws.WSResponse;
+import scala.concurrent.ExecutionContextExecutor;
+import scala.concurrent.duration.Duration;
+import scala.concurrent.duration.FiniteDuration;
+
+public class WebServiceHealthMonitor extends AbstractActor
+{
+  private WSClient wsclient;
+  private ActorRef sender;
+  private ActorRef self;
+
+
+  public static class MonitorHealth
+  {
     public String url;
 
-    public MonitorHealth(String url) {
+
+    public MonitorHealth(String url)
+    {
       this.url = url;
     }
+  }
+
+  public static class CheckIfStillAlive
+  {
+  }
+
+  public static class Alive
+  {
+  }
+
+  public static class Dead
+  {
+  }
+
+
+  public WebServiceHealthMonitor(WSClient wsclient)
+  {
+    this.wsclient = wsclient;
+    receive(ReceiveBuilder.match(MonitorHealth.class, this::monitorHealth)
+        .build());
+  }
+
+
+  public void monitorHealth(MonitorHealth monitorHealth)
+  {
+    Alive alive = new Alive();
+    Promise< WSResponse > wsResponse = wsclient.url(monitorHealth.url)
+        .setRequestTimeout(100)
+        .get();
+    sender = sender();
+    self = self();
+    ActorContext context = context();
+    Scheduler scheduler = context().system().scheduler();
+    ExecutionContextExecutor dispatcher = context().system().dispatcher();
+    wsResponse.onFailure(t -> sender.tell(new Dead(), self()));
+    wsResponse.onRedeem(r -> {
+      sender.tell(new Alive(), self());
+      scheduler.scheduleOnce(FiniteDuration.create(500, TimeUnit.MILLISECONDS), self, context.dispatcher());
+      
+    });
   }
 }
--- src/test/java/de/dfs/servicemonitor/monitor/WebServiceHealthMonitorTest.java
+++ src/test/java/de/dfs/servicemonitor/monitor/WebServiceHealthMonitorTest.java
@@ -1,5 +1,7 @@
 package de.dfs.servicemonitor.monitor;
 
+import static play.mvc.Results.ok;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -10,25 +12,53 @@ import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.testkit.JavaTestKit;
 import akka.testkit.TestActorRef;
+import de.dfs.servicemonitor.monitor.WebServiceHealthMonitor.Alive;
+import de.dfs.servicemonitor.monitor.WebServiceHealthMonitor.Dead;
 import de.dfs.servicemonitor.monitor.WebServiceHealthMonitor.MonitorHealth;
+import play.api.routing.Router;
+import play.libs.ws.WS;
+import play.libs.ws.WSClient;
+import play.routing.RoutingDsl;
+import play.server.Server;
 
-public class WebServiceHealthMonitorTest {
- private static final String MONITOR = "monitor";
-private TestActorRef< Actor > monitorActorRef;
-private ActorSystem system;
-private JavaTestKit testKit;
-  
+public class WebServiceHealthMonitorTest
+{
+  private static final String MONITOR = "monitor";
+  private TestActorRef< Actor > monitorActorRef;
+  private ActorSystem system;
+  private JavaTestKit testKit;
+  private WSClient ws;
+  private Server server;
+  private WSClient timeOutWs;
+
+
   @BeforeClass
   public static void setupClass()
   {
   }
-  
-  
-  @Before
-  public void setup() {
-    system = ActorSystem.create();  
-    testKit = new JavaTestKit(system);
 
+
+  @Before
+  public void setup()
+  {
+    system = ActorSystem.create();
+    testKit = new JavaTestKit(system);
+    Router router = new RoutingDsl().GET("/")
+        .routeTo(() -> {
+          return ok("alive");
+        })
+        .build();
+    server = Server.forRouter(router);
+    ws = WS.newClient(server.httpPort());
+
+    Router timeoutRouter = new RoutingDsl().GET("/")
+        .routeTo(() -> {
+          Thread.sleep(20000);
+          return ok("alive");
+        })
+        .build();
+   Server timoutServer = Server.forRouter(timeoutRouter);
+   timeOutWs = WS.newClient(timoutServer.httpPort());
   }
 
 
@@ -38,13 +68,28 @@ private JavaTestKit testKit;
     JavaTestKit.shutdownActorSystem(system);
     system = null;
   }
+
+
+  @Test
+  public void normalFlow()
+  {
+    Props socketPublishingMonitorProps = Props.create(WebServiceHealthMonitor.class, ws);
+    monitorActorRef = TestActorRef.create(system, socketPublishingMonitorProps, MONITOR);
+    MonitorHealth monitorHealth = new WebServiceHealthMonitor.MonitorHealth("/");
+    monitorActorRef.tell(monitorHealth, testKit.getRef());
+    testKit.expectMsgClass(Alive.class);
+    testKit.expectMsgClass(Alive.class);
+
+  }
   
   @Test
-  public void normalFlow() {
-    Props socketPublishingMonitorProps = Props.create(WebServiceHealthMonitor.class);
+  public void timeoutFlow()
+  {
+    Props socketPublishingMonitorProps = Props.create(WebServiceHealthMonitor.class, timeOutWs);
     monitorActorRef = TestActorRef.create(system, socketPublishingMonitorProps, MONITOR);
-    MonitorHealth monitorHealth = new WebServiceHealthMonitor.MonitorHealth("SOCKET");
+    MonitorHealth monitorHealth = new WebServiceHealthMonitor.MonitorHealth("/");
     monitorActorRef.tell(monitorHealth, testKit.getRef());
-    
+    testKit.expectMsgClass(Dead.class);
+
   }
 }
